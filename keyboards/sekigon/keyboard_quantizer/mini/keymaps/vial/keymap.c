95cccf38a5d37813999f2ea72340b37e1749755b
pc-atom <tks_atom@yahoo.co.jp>
2026-08-23 02:22:14 +0900

    Implement LED update on caps lock toggle
    Add LED update function for caps lock state.

--- keyboards/sekigon/keyboard_quantizer/mini/keymaps/vial/keymap.c
+++ keyboards/sekigon/keyboard_quantizer/mini/keymaps/vial/keymap.c
@@ -372,3 +372,12 @@ void dynamic_keymap_macro_send(uint8_t id) {
         }
     }
 }
+
+bool led_update_user(led_t led_state) {
+    if (led_state.caps_lock) {
+        writePinLow(GP25);
+    } else {
+        writePinHigh(GP25);
+    }
+    return true;
+}
